9cf4ac5d7fb606226695e95bc88ee664ce47bc4f
HardyValen <hardyvalen@gmail.com>
2020-10-17 04:51:15 +0700

    testing cors on backend

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
+// CORS options
+var corsOptions = {
+  origin: ['http://localhost:3000','https://hardyvalen.azurewebsites.net'],
+  optionsSuccessStatus: 200,
+  methods: "GET,POST",
+}
 
 // Connect to database
 const sequelize = require("./db/connect");
@@ -16,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/', require("./routes/index"));
 
