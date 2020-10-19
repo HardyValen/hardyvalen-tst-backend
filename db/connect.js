03cb19e1919ca354d9c30e94632a50e74d0dbeda
HardyValen <hardyvalen@gmail.com>
2020-10-19 13:26:35 +0700

    changed database

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,10 @@ const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
   "postgres",
-  "penguasa@basisdata",
+  "penguasa@basisdata2",
   "1E2A3D4a5c6a9412",
   {
-    host: "basisdata.postgres.database.azure.com",
+    host: "basisdata2.postgres.database.azure.com",
     dialect: "postgres",
     timezone: "+07:00",
   }
